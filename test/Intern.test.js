bbf0b023524def36ba6925794ae4c64fdbd7dbba
Nicholas Marrone <nmarrone1997@gmail.com>
2021-03-11 12:40:49 -0500

    Built out intern test

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -3,13 +3,19 @@
 const Intern = require("../lib/Intern");
 
 test("Can set school via constructor", () => {
-
+    const value = "UNCC";
+    const emp = new Intern("Josh", 1, "josh@gmail.com", value);
+    expect(emp.school).toBe(value);
 });
 
 test("getRole() should return \"Intern\"", () => {
-
+    const value = "Intern";
+    const emp = new Intern("Lauren", 1, "lauren@gmail.com", "UNCC");
+    expect(emp.getRole()).toBe(value);
 });
 
 test("Can get school via getSchool()", () => {
-
+    const value = "UNCC";
+    const emp = new Intern("Kiwi", 1, "kiwi@gmail.com", value);
+    expect(emp.getSchool()).toBe(value);
 });
