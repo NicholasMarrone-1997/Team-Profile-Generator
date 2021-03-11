a8a80a64906f8447808d1f57018523c6918f70f3
Nicholas Marrone <nmarrone1997@gmail.com>
2021-03-11 14:06:12 -0500

    Built out tests

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -4,13 +4,19 @@ const Manager = require("../lib/Manager");
 const Employee = require("../lib/Employee");
 
 test("Can set office number via constructor argument", () => {
-
+    const value = 100;
+    const emp = new Manager("Eric", 1, "eric@gmail.com", value);
+    expect(emp.officeNumber).toBe(value);
 });
 
 test('getRole() should return "Manager"', () => {
-
+    const value = "Manager";
+    const emp = new Manager("Kaiba", 1, "kaiba@seto.com", 101);
+    expect(emp.getRole()).toBe(value);
 });
 
 test("Can get office number via getOffice()", () => {
-
+    const value = 100;
+    const emp = new Manager("Larry", 1, "larry@gmail.com", value);
+    expect(emp.getOfficeNumber()).toBe(value);
 });
